be73bc4264cef85137dadd1bf7e9604edf78c931
Dmitry S <vmike1337v@gmail.com>
2025-03-30 18:06:00 +0200

    Boilerplate wip

---
+++ src/args.rs
@@ -0,0 +1,44 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct CLI {
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Encrypt(EncryptArgs),
+    Decrypt(DecryptArgs),
+    Name(NameArgs),
+    QR(QRArgs),
+}
+
+#[derive(Parser)]
+#[clap(visible_aliases = ["e", "encode"])]
+pub struct EncryptArgs {
+    /// Text to encrypt
+    pub text: String,
+}
+
+#[derive(Parser)]
+#[clap(visible_aliases = ["d", "decode"])]
+pub struct DecryptArgs {
+    /// Text to decrypt
+    pub text: String,
+}
+
+#[derive(Parser)]
+#[clap(visible_aliases = ["n", "label"])]
+/// Generate random name
+pub struct NameArgs {
+    /// Number of names to generate
+    pub times: u32,
+}
+
+#[derive(Parser)]
+/// Generate QR code
+pub struct QRArgs {
+    /// Text to encode in QR code
+    pub text: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod args;
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = args::CLI::parse();
+
+    match cli.command {
+        args::Command::Encrypt(args) => encrypt(args),
+        args::Command::Decrypt(args) => decrypt(args),
+        args::Command::Name(args) => generate_name(args),
+        args::Command::QR(args) => generate_qr(args),
+    };
+
+    // todo handle errors
+    // todo use stdin (optionally)
+    // todo use file to write (optionally)
+}
+
+fn encrypt(args: args::EncryptArgs) {
+    println!("Calling encrypt({})", args.text);
+}
+
+fn decrypt(args: args::DecryptArgs) {
+    println!("Calling decrypt({})", args.text);
+}
+
+fn generate_name(args: args::NameArgs) {
+    println!("Calling generate_name(times={})", args.times);
+}
+
+fn generate_qr(args: args::QRArgs) {
+    println!("Calling generate_qr(text={})", args.text);
 }
